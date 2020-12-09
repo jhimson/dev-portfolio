456b2fc333d8e11ea5ce345eca5382bd772a2491
jhimson <jhimsonrayraboypamisa@gmail.com>
2020-12-08 19:51:06 -0500

    Some header adjustments

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         <div className='flex flex-col items-center justify-center' style={{backgroundColor: '#091c29'}}>
             <div className="flex flex-col min-h-screen lg:flex-row-reverse">
                 <div className='lg:ml-72'>
-                    <LazyLoadImage src={DevImage} effect="blur" className='rounded-lg opacity-40 lg:w-4/5 lg:h-4/5'/>
+                    <LazyLoadImage src={DevImage} effect="blur" className='w-full rounded-lg opacity-40 lg:w-4/5 lg:h-4/5'/>
                 </div>
                 <div className='flex flex-col items-center justify-center text-center text-white font-Dosis'>
                     <div className='my-4 text-3xl font-bold lg:text-8xl'>
@@ -26,7 +26,7 @@ const Header = () => {
                         />
                     </div>
                     <div>
-                        <button className='px-6 py-2 mt-4 text-2xl bg-indigo-600 rounded-lg animate-float'>Discover More</button>
+                        <button className='px-10 py-2 mt-4 text-2xl bg-indigo-600 rounded-lg animate-float'>Discover More</button>
                     </div>
                 </div>
             </div>
