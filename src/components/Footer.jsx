32ca969d63a9c5c54b09869d66a0501782848c27
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-08-01 21:03:11 -0400

    feat: create footer component (initial design)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+    const x = 0
+    return (
+        <div className='h-36' style={{backgroundColor: '#091c29'}}>
+            <h1>Footer</h1>
+        </div>
+    )
+}
+
+export default Footer
