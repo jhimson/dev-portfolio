284acdbfbd693e82b4e68f6b15055f0583b46b8b
jhimson <jhimsonrayraboypamisa@gmail.com>
2021-08-03 23:16:52 -0400

    style: add hover color for social media icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
     return (
         <div className='flex flex-col items-center justify-center space-y-5 h-36' style={{backgroundColor: '#091c29'}}>
             <div className="flex space-x-5 icons">
-                <span className='text-gray-200'><FaGithub size='1.5em'/></span>
-                <span className='text-gray-200'><FaLinkedin size='1.5em'/></span>
-                <span className='text-gray-200'><FaTwitter size='1.5em'/></span>
-                <span className='text-gray-200'><FaFacebookSquare size='1.5em'/></span>
-                <span className='text-gray-200'><FaInstagram size='1.5em'/></span>
+                <span className='text-gray-200 cursor-pointer hover:text-purple-400'><FaGithub size='1.5em'/></span>
+                <span className='text-gray-200 cursor-pointer hover:text-blue-600'><FaLinkedin size='1.5em'/></span>
+                <span className='text-gray-200 cursor-pointer hover:text-blue-300'><FaTwitter size='1.5em'/></span>
+                <span className='text-gray-200 cursor-pointer hover:text-blue-500'><FaFacebookSquare size='1.5em'/></span>
+                <span className='text-gray-200 cursor-pointer hover:text-yellow-400'><FaInstagram size='1.5em'/></span>
 
             </div>
             <div className='text-xl text-gray-200 copyrights'><p>&copy; 2021 All rights reserved. – Designed & Coded with 💖 by Jhimson Pamisa</p></div>
